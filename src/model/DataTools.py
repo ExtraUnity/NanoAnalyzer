Fixed bug with diameter calculation

src/model/DataTools.py
@@ -199,7 +199,10 @@ def get_dataloaders_without_testset(dataset: Dataset, train_data_size: float, in
         train_data = data_augmenter.augment_dataset(train_data, input_size, [False, False, False, False, False, False, False])
     val_data = process_and_slice(val_data, input_size)
 
-    train_dataloader = DataLoader(train_data, batch_size=32, shuffle=True, drop_last=True)
+    if torch.cuda.is_available():
+        train_dataloader = DataLoader(train_data, batch_size=32, shuffle=True, drop_last=True, num_workers=24)
+    else:
+        train_dataloader = DataLoader(train_data, batch_size=8, shuffle=True, drop_last=True)
     val_dataloader = DataLoader(val_data, batch_size=1, shuffle=True, drop_last=True)
     return (train_dataloader, val_dataloader)
 

src/model/ModelEvaluator.py
@@ -125,7 +125,7 @@ class ModelEvaluator():
         number_of_predictions_to_show = np.min([5, len(predictions)]) 
         indicies = random.sample(range(len(predictions)), number_of_predictions_to_show)
         if not test_callback:
-            return np.mean(ious), np.mean(dice_scores)
+            return EvaluationResult(ious, dice_scores)
         try:
             for i in indicies:
                 test_callback(inputs[i], predictions[i], labels[i], ious[i], dice_scores[i])

src/model/StatsWriter.py
@@ -32,12 +32,13 @@ class StatsWriter:
         return nm_value / self._unit_conversion_factors.get(to_unit, 1)
     
     def __get_diameters(self, stats):
+        """Calculate diameters using Equivalent Circular Diameter (ECD)."""
         import numpy as np
-        from cv2 import CC_STAT_HEIGHT, CC_STAT_WIDTH
+        from cv2 import CC_STAT_AREA
         diameters = np.empty(stats.shape[0]-1)
         for label_idx in range(1, stats.shape[0]):
-            width, height = stats[label_idx, CC_STAT_WIDTH], stats[label_idx, CC_STAT_HEIGHT]
-            diameter = np.mean([width, height])  
+            area = stats[label_idx, CC_STAT_AREA]
+            diameter = 2 * np.sqrt(area / np.pi)
             diameters[label_idx-1] = diameter
         return diameters
     